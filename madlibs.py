f751ebc2dd202edd873efb7c30323c7b7191df98
Émery Freitas Moriconi <119318857+emerymoriconi@users.noreply.github.com>
2023-07-23 14:34:30 -0300

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,4 @@
-#MadLibs é um jogo divertido e engraçado no qual o usuário insere \ certas palavras que completarão os espaços em branco de um texto desconhecido pelo usuário, gerando um texto aleatório. 
+#MadLibs é um jogo divertido e engraçado no qual o usuário insere \ certas palavras que completarão os espaços em branco de um texto \ desconhecido pelo usuário, gerando um texto aleatório. 
 
 adjetivo1 = input("Adjetivo1: ")
 adjetivo2 = input("Adjetivo2: ")
@@ -10,14 +10,6 @@ substantivo2 = input("Substantivo2: ")
 substantivo3 = input("Substantivo3: ")
 substantivo4 = input("Substantivo4: ")
 
-madlibs = f"Exercitar habilidades em programação não é uma \
-    tarefa {adjetivo1}, mas é bastante compensatória. Me sinto \
-        bem exercitanto Python e {verbo1} um pouco mais a cada dia. \
-            Além de {verbo2}, gosto bastante de praticar esportes, \
-                ver {substantivo1} e ouvir Taylor Swift. Ela é a \
-                    maior {adjetivo2} de todos os tempos. Durante os dias, \
-                        busco me manter focada, {adjetivo3} e cuido muito da minha alimentação. \
-                            Bem, na medida do possível, já que não dispenso um {substantivo2}. \
-                                Então, obrigada por visitar meu {substantivo3} e boa sorte na sua {substantivo4}."
+madlibs = f"Exercitar habilidades em programação não é uma tarefa {adjetivo1}, mas é bastante compensatória. Me sinto bem exercitanto Python e {verbo1} um pouco mais a cada dia. Além de {verbo2}, gosto bastante de praticar esportes, ver {substantivo1} e ouvir Taylor Swift. Ela é a maior {adjetivo2} de todos os tempos. Durante os dias, busco me manter focada, {adjetivo3} e cuido muito da minha alimentação. Bem, na medida do possível, já que não dispenso um {substantivo2}. Então, obrigada por visitar meu {substantivo3} e boa sorte na sua {substantivo4}."
 
 print(madlibs)
