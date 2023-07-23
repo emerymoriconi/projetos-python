dfd671b75512de7cb06d9773bb6613d60b31d1cb
Émery Freitas Moriconi <119318857+emerymoriconi@users.noreply.github.com>
2023-07-23 14:27:44 -0300

    Projeto MadLibs

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+#MadLibs é um jogo divertido e engraçado no qual o usuário insere certas palavras que completarão os espaços em branco de um texto desconhecido pelo usuário, gerando um texto aleatório. 
+
+adjetivo1 = input("Adjetivo1: ")
+adjetivo2 = input("Adjetivo2: ")
+adjetivo3 = input("Adjetivo3: ")
+verbo1 = input("Verbo1: ")
+verbo2 = input("Verbo2: ")
+substantivo1 = input("Substantivo1: ")
+substantivo2 = input("Substantivo2: ")
+substantivo3 = input("Substantivo3: ")
+substantivo4 = input("Substantivo4: ")
+
+madlibs = f"Exercitar habilidades em programação não é uma tarefa {adjetivo1}, mas é bastante compensatória. Me sinto bem exercitanto Python e {verbo1} um pouco mais a cada dia. Além de {verbo2}, gosto bastante de praticar esportes, ver {substantivo1} e ouvir Taylor Swift. Ela é a maior {adjetivo2} de todos os tempos. Durante os dias, busco me manter focada, {adjetivo3} e cuido muito da minha alimentação. Bem, na medida do possível, já que não dispenso um {substantivo2}. Então, obrigada por visitar meu {substantivo3} e boa sorte na sua {substantivo4}."
+
+print(madlibs)
